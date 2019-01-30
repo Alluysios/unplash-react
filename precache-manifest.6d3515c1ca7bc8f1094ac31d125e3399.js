fea7b616b7a9b227008fc35a055b42259c38f6ad
Alluysios Arriba <alluysiosarriba@Alluysioss-MacBook-Air.local>
2019-01-29 20:29:23 -0700

    Updates

---
+++ precache-manifest.6d3515c1ca7bc8f1094ac31d125e3399.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ad33f5b675e8e8cfe4a",
+    "url": "/unplash-react/static/js/runtime~main.0ad33f5b.js"
+  },
+  {
+    "revision": "c1eb74c72bb6535e0b1c",
+    "url": "/unplash-react/static/js/main.c1eb74c7.chunk.js"
+  },
+  {
+    "revision": "102c3bf24ee7bc42981e",
+    "url": "/unplash-react/static/js/1.102c3bf2.chunk.js"
+  },
+  {
+    "revision": "c1eb74c72bb6535e0b1c",
+    "url": "/unplash-react/static/css/main.5eb9f3a0.chunk.css"
+  },
+  {
+    "revision": "2accb1f32afa5dc60fc46f8192ce2425",
+    "url": "/unplash-react/index.html"
+  }
+];
--- src/api/unsplash.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-// 50 calls per day
-export default axios.create({
-    baseURL: 'https://api.unsplash.com',
-    headers: {
-        Authorization: 'Client-ID af26898466eb73e17bdb4ba6c4a62b1cb7460998fb9962d93f4256b2e15cbea6'
-    }
-});
